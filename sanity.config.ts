0668e8c9fc110018f8192f7de099b92cd4e6ddcb
Matthew Rhoads <78577112+mematthew123@users.noreply.github.com>
2024-07-18 10:28:32 -0600

    updated sstudio structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,16 +11,45 @@ import {structureTool} from 'sanity/structure'
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schema'
-import { publishAndSendPlugin } from './sanity/lib/publishAndSendPlugin'
+import {publishAndSendPlugin} from './sanity/lib/publishAndSendPlugin'
+
+
+
+
+const structure = (S: any) =>
+  S.list()
+    .title('Content')
+    .items([
+      // Add the "Emails" folder with the specific document types
+      S.listItem()
+        .title('Emails')
+        .child(
+          S.list()
+            .title('Emails')
+            .items([
+              S.documentTypeListItem('newsletter').title('Newsletter'),
+              S.documentTypeListItem('emailSignUp').title('Email Sign Up'),
+              S.documentTypeListItem('contacts').title('Contacts'),
+            ])
+        ),
+      // Add other default items
+      ...S.documentTypeListItems().filter(
+        (listItem: { getId: () => string }) =>
+          !['newsLetter', 'emailSignUp', 'contacts'].includes(listItem.getId())
+      ),
+    ]);
+
+
 
 export default defineConfig({
+  
   basePath: '/studio',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    structureTool(),
+    structureTool({ structure }), 
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
